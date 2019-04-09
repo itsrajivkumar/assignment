8861d5a0bf44d2efda567460909a326e3eef5eaf
Rajiv <rajiv.singh@mediaplex.co.in>
2019-04-09 19:03:57 +0530

    router changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,27 +3,77 @@ const auth = require('../middleware/auth')
 
 
 
-    var createUser= async (req, res) => {
-        const user = new UserModel(req.body)
+var createUser = async (req, res) => {
+    const user = new UserModel(req.body)
 
-        try {
-            await user.save()
-            const token = await user.generateAuthToken()
-            res.status(201).send({ user, token })
-        } catch (e) {
-            res.status(400).send(e)
-        }
-    },
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+}
 
-    var login = async (req, res) => {
-        try {
-            const user = await UserModel.findByCredentials(req.body.email, req.body.password)
-            const token = await user.generateAuthToken()
-            res.send({ user, token })
-        } catch (e) {
-            res.status(400).send()
-        }
+var login = async (req, res) => {
+    try {
+        const user = await UserModel.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+}
+var logout = async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+}
+
+var logoutAll = async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
     }
 
+}
+var myProfile = async (req, res) => {
+    res.send(req.user)
+}
+var updateProfile =  async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'dateOfBirth']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-module.exports = createUser,login
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+}
+
+var deleteProfile =  async (req, res) => {
+    try {
+        await req.user.remove()
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
+}
+module.exports = { createUser, login, logout, logoutAll,myProfile,updateProfile,deleteProfile}
--- src/routers/user.js
+++ src/routers/user.js
@@ -8,58 +8,14 @@ router.post('/users',User.createUser)
 
 router.post('/users/login',User.login)
 
-router.post('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
-        await req.user.save()
+router.post('/users/logout', auth,User.logout)
 
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+router.post('/users/logoutAll', auth, User.logoutAll)
 
-router.post('/users/logoutAll', auth, async (req, res) => {
-    try {
-        req.user.tokens = []
-        await req.user.save()
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+router.get('/users/me', auth, User.myProfile)
 
-router.get('/users/me', auth, async (req, res) => {
-    res.send(req.user)
-})
+router.patch('/users/me', auth,User.updateProfile)
 
-router.patch('/users/me', auth, async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-
-    try {
-        updates.forEach((update) => req.user[update] = req.body[update])
-        await req.user.save()
-        res.send(req.user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-router.delete('/users/me', auth, async (req, res) => {
-    try {
-        await req.user.remove()
-        res.send(req.user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+router.delete('/users/me', auth,User.deleteProfile)
 
 module.exports = router
